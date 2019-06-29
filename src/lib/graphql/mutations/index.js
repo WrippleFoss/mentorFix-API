dcad04c0497a443b3b76dca3ad30287af18efe6e
abhishek71994 <abhishek71994@gmail.com>
2019-06-29 11:27:04 +0800

    added mentee to DB

--- src/lib/graphql/mutations/index.js
+++ src/lib/graphql/mutations/index.js
@@ -1,10 +1,12 @@
 import { GraphQLObjectType } from 'graphql';
 import AddMentor from './mentor/addMentor';
+import AddMentee from './mentee/addMentee';
 
 const rootMutation = new GraphQLObjectType({
   name: 'RootMutation',
   fields: {
-    addMentor: AddMentor
+    addMentor: AddMentor,
+    addMentee: AddMentee
   }
 });
 
