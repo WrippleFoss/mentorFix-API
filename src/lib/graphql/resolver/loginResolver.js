9433479ee266fd5ff7cdf84ba9dbb79c47a27c64
abhishek71994 <abhishek71994@gmail.com>
2019-07-16 17:05:41 +0800

    authentication updated, working authentication with token

--- src/lib/graphql/resolver/loginResolver.js
+++ src/lib/graphql/resolver/loginResolver.js
@@ -3,24 +3,22 @@ const menteeModel = require('../../database/models/mentee');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const LoginResolver = async args => {
-  console.log(args);
-  const mentee = menteeModel.findOne({ email: args.email });
+const LoginResolver = async (parent, args) => {
+  const mentee = await menteeModel.findOne({ email: args.email });
   if (!mentee) {
     throw new Error('No such user exists');
   }
-  const isEqual = await bcrypt.compare(args.password, user.password);
-
+  const isEqual = await bcrypt.compare(args.password, mentee.password);
   if (!isEqual) {
     throw new Error('Password is incorrect');
   }
   const token = jwt.sign(
-    { userID: user.id, email: user.email },
+    { userID: mentee.id, email: mentee.email },
     'supersecretkey',
     { expiresIn: '1h' }
   ); //later needs to have a env based secret
   return {
-    userId: user.id,
+    userId: mentee.id,
     token: token,
     tokenExpiration: 1
   };
